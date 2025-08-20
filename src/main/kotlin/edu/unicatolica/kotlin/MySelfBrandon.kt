60c6672443c0d542f866fd1b6c0bb6c0bf703c49
brandonstevenz2002 <brandonstevenvm@gmail.com>
2025-08-20 14:42:21 -0500

    Agregue las variablesYtipos y tambien el taller Myself

---
+++ src/main/kotlin/edu/unicatolica/kotlin/MySelfBrandon.kt
@@ -0,0 +1,11 @@
+package edu.unicatolica.kotlin
+
+fun main (){
+    val name: String = "Brandon Steven Vargas Martinez"
+    val age: Int = 23
+    val program: String = "Tec.Desarrollo de software"
+    val interest: String = "Mejorar mis habilidades en Programacion"
+
+    val message = "Hola mi nombre es $name, tengo $age años y estoy en el programa $program, mi interes es $interest"
+    println(message)
+}
--- src/main/kotlin/edu/unicatolica/kotlin/VariablesYTipos.kt
+++ src/main/kotlin/edu/unicatolica/kotlin/VariablesYTipos.kt
@@ -3,4 +3,28 @@ package edu.unicatolica.kotlin
 
 fun main() {
     // variables and data types
+    println("Hola mundo")
+
+    //val name1 = "Brandon"
+    //val name2 = "Vargas"
+
+//    val number1: Int = 1
+    //  val number2: Long = 3L
+    //val number3: Float = 3.14f
+    //val number4: Double = 2.4
+
+    //val adding = 10 + 1
+    //val subtraction = 10 - 1
+    //val product = 10 * 1
+    //val division =10 / 1
+    //val modulating =10 % 1
+
+    val name3 = "Lina"
+    val age = 19
+    val city = "Palmira"
+
+    val message = "Hola, $name3. Your age is $age, and you live in $city"
+    println(message)
+
+
 }
